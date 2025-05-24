4ed94fbecd68be8d13d8e647eb34df247ef9a4fe
Roberto-S-C <robertosanchezcazares@gmail.com>
2025-05-24 08:07:06 -0600

    Added Styles

---
+++ constants/Colors.ts
@@ -0,0 +1,11 @@
+const Colors = {
+  white: "#FFFFFF",
+  creamWhite: "#FEFCF8",
+  lightBlue: "#73D5E4",
+  darkBlue: "#0F476A",
+  lightGrey: "#DDDDDD",
+  darkGrey: "#C1C1C1",
+  red: "#A42F2F",
+};
+
+export default Colors;
---
+++ styles/globalStyles.ts
@@ -0,0 +1,34 @@
+import Colors from '@/constants/Colors';
+import { StyleSheet } from 'react-native';
+
+export const globalStyles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    alignItems: 'center',
+    backgroundColor: Colors.creamWhite,
+  },
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    width: '95%',
+  },
+  border:
+  {
+    borderWidth: 1,
+    borderColor: Colors.lightGrey,
+    borderRadius: 10,
+    padding: 10,
+  },
+  controlButton: 
+  {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 5,
+    backgroundColor: Colors.white,
+    borderWidth: 1,
+    borderRadius: 10,
+    borderColor: Colors.lightGrey,
+    padding: 5,
+    fontSize: 16,
+  }
+});
