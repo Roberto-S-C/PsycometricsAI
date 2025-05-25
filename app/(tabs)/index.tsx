0d0cc4694dcba78ac6cd3144148d80d68f5a74f2
Roberto-S-C <robertosanchezcazares@gmail.com>
2025-05-25 14:08:21 -0600

    Added Reports UI

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,13 @@
-import { Text, View } from "react-native";
+import BarChartComponent from "@/components/Charts/BarChart";
+import PieChartComponent from "@/components/Charts/PieChart";
+import { globalStyles } from "@/styles/globalStyles";
+import { View } from "react-native";
 
 export default function Index() {
   return (
-    <View>
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={globalStyles.screen}>
+      <BarChartComponent />
+      <PieChartComponent />
     </View>
   );
 }
